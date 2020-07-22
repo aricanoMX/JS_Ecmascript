3556258049cf9eddd8f23f2b3b0dc4a9ce34ebed
Andrés Ricaño <aricanomx@gmail.com>
2020-07-21 23:09:04 -0500

    clase 3

--- index.js
+++ index.js
@@ -44,3 +44,61 @@ let lorem2 = `ahora lo que tenemos que hacer es solamente
 saltar linea y automaticamente se actualizara a un salto de linea normal.
 `;
 console.log(lorem2);
+// ___________________________________
+// Destructuracion de elementos
+// ___________________________________
+
+// ES5
+let person = {
+  name: "Oscar",
+  age: 32,
+  country: "MX",
+};
+console.log(person.name, person.age, person.country);
+
+// ES6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// ___________________________________
+// Propagacion
+// ___________________________________
+
+let team1 = ["Oscar", "Julian", "Armando"];
+let team2 = ["Mirella", "Linda", "Rosalva"];
+
+// ES5
+let education = [
+  "David",
+  "Oscar",
+  "Julian",
+  "Armando",
+  "Mirella",
+  "Linda",
+  "Rosalva",
+];
+console.log(education);
+
+// ES6
+let education2 = ["David", ...team1, ...team2];
+console.log(education2);
+
+// Variables y Scope global y local
+
+{
+  var global = "Global Var";
+}
+
+{
+  let globalet = "Global Let";
+  console.log(globalet);
+}
+
+console.log(global);
+
+const a = "b";
+a = "a";
+console.log(a);
+var a = "b";
+a = "a";
+console.log(a);
